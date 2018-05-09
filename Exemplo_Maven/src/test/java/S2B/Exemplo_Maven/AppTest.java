79a183c2a39123b08e38ddd5d38c1aa69f72fe44
Lucas Plautz Prestes <lucas.plautz.prestes@gmail.com>
2018-05-09 14:15:33 -0300

    Template_Maven
    Add template Mavem

---
+++ Exemplo_Maven/src/test/java/S2B/Exemplo_Maven/AppTest.java
@@ -0,0 +1,14 @@
+package S2B.Exemplo_Maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
